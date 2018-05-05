a3dbd1cc617d87b6b0c43ec2e3b2bd9b742f09fc
小倉優悟 <jigenbakdaaan@gmail.com>
2018-05-05 15:36:44 +0900

    [update] pwaに対応

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,30 +1,63 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: "dock-hack",
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "Nuxt.js project" }
+    meta: [{
+        charset: "utf-8"
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
+      },
+      {
+        hid: "description",
+        name: "description",
+        content: "Nuxt.js project"
+      }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{
+      rel: "icon",
+      type: "image/x-icon",
+      href: "/favicon.ico"
+    }]
   },
   /*
-  ** Customize the progress bar color
-  */
-  loading: { color: "#FFFFFF" },
-  modules: ["@nuxtjs/axios"],
+   ** Customize the progress bar color
+   */
+  loading: {
+    color: "#FFFFFF"
+  },
+  modules: ["@nuxtjs/axios",
+    '@nuxtjs/pwa'
+  ],
+  workbox: {
+    dev: true,
+  },
   mode: "spa",
+  manifest: {
+    name: 'DockHack',
+    short_name: 'DH',
+    title: 'DockHack',
+    'og:title': 'DockHack',
+    description: 'DockHack',
+    'og:description': 'DockHack',
+    lang: 'ja',
+    theme_color: '#2b5876',
+    background_color: '#2b5876'
+  },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** Run ESLint on save
-    */
-    extend(config, { isDev, isClient }) {
+     ** Run ESLint on save
+     */
+    extend(config, {
+      isDev,
+      isClient
+    }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: "pre",
