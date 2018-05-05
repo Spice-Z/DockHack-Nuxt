d30d7420e1c6489a002025b91eecf45bc27149bb
小倉優悟 <jigenbakdaaan@gmail.com>
2018-05-05 15:25:23 +0900

    [fix] mentionIdeaのパラメータ修正

--- pages/IDetail.vue
+++ pages/IDetail.vue
@@ -3,7 +3,7 @@
     <Idea v-for="(value, key) in getUpperIdeas()" v-bind:key="key" v-bind:value="value" v-bind:class="{'second-box':(key!=0)}" class="idea-box" />
     <FocusedIdea class="idea-box FIdea-box" v-bind:idea="getFIdea()"/>
     <Idea v-for="(value, key) in getUnderIdeas()" v-bind:key="key+30" v-bind:value="value" class="idea-box second-box" />
-    <router-link :to="{ path: '/Tweet' , query: { FIdeaId: getLastIdeaId() } }" class="mentionButton">+</router-link>
+    <router-link :to="{ path: '/Tweet' , query: { mentionToId: getLastIdeaId() } }" class="mentionButton">+</router-link>
   </div>
 </template>
 
